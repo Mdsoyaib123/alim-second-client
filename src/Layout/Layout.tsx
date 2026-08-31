167be634a548e8b481a6ba6f4790de0f662b4b5d
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-31 08:40:35 +0600

    feat: scroll to top on route change

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,9 +1,17 @@
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Toaster } from "sonner";
 
 const Layout: React.FC = () => {
+  const { pathname } = useLocation();
+
+  // Scroll to top automatically whenever the route changes
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
